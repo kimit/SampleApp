a0fb20d3c76f2c1260e1679b0033a32b9b8d80ee
toshiHome <sebalyn@mac.com>
2013-10-18 22:11:51 -0700

    Initial Commit

---
+++ SampleApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by toshi on 2013/10/18.
+//  Copyright (c) 2013年 toshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
